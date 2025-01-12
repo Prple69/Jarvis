fc5aa39e03b32ba145d3f0cfdbebae767f4ea019
Matthew <94802922+Prple69@users.noreply.github.com>
2025-01-12 21:02:23 +0700

    Update config_manager.py

--- config_manager.py
+++ config_manager.py
@@ -1,8 +1,8 @@
 import telebot
 
-TOKEN_LICENSE= '5091305709:AAHEELKwqXgIrH8YwZn9zl9ZgIVzzfOZiVU'
+TOKEN_LICENSE= ''
 BOT_LICENSE= telebot.TeleBot(TOKEN_LICENSE)
-BOT_INFO = telebot.TeleBot('6042458044:AAF8j4Vak113ljX7ma8dyYOcVM7umNTetX4')
+BOT_INFO = telebot.TeleBot('')
 
 NIRCMD = r'bin\\nircmd.exe'
 
